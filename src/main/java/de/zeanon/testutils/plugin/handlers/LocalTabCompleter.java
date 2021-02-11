10d94b4948757d20e14790e69cec04b79e038429
Zeanon <thezeanon@gmail.com>
2021-02-11 02:45:07 +0100

    updated TabCompletion
    Signed-off-by: Zeanon <thezeanon@gmail.com>

--- src/main/java/de/zeanon/testutils/plugin/handlers/LocalTabCompleter.java
+++ src/main/java/de/zeanon/testutils/plugin/handlers/LocalTabCompleter.java
@@ -5,7 +5,6 @@ import de.zeanon.storagemanagercore.internal.utility.basic.BaseFileUtils;
 import de.zeanon.testutils.TestUtils;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import org.bukkit.command.Command;
@@ -19,27 +18,7 @@ public class LocalTabCompleter implements TabCompleter {
 
 	@Override
 	public List<String> onTabComplete(final @NotNull CommandSender sender, final @NotNull Command command, final @NotNull String alias, final @NotNull String @NotNull [] args) {
-		if (args.length == 0) {
-			if (command.getName().equalsIgnoreCase("tnt")) {
-				return Arrays.asList("allow", "deny", "other", "info");
-			} else if (command.getName().equalsIgnoreCase("testblock")) {
-				final @NotNull List<String> completions = Arrays.asList("undo", "here");
-				try {
-					File tempDirectory = new File(TestUtils.getInstance().getDataFolder().getAbsolutePath() + "/Blocks/" + ((Player) sender).getUniqueId().toString());
-					if (tempDirectory.exists() && tempDirectory.isDirectory()) {
-						for (File tempFile : BaseFileUtils.listFiles(tempDirectory)) {
-							completions.add(BaseFileUtils.removeExtension(tempFile.getName()));
-						}
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-				return completions;
-			} else if (command.getName().equalsIgnoreCase("testutils")) {
-				return Arrays.asList("registerblock", "deleteblock", "registertg", "update");
-			}
-		} else if (args.length == 1) {
+		if (args.length == 1) {
 			if (command.getName().equalsIgnoreCase("tnt")) {
 				return this.getCompletions(args[0], "allow", "deny", "other", "info");
 			} else if (command.getName().equalsIgnoreCase("testblock")) {
@@ -70,8 +49,12 @@ public class LocalTabCompleter implements TabCompleter {
 				}
 			} else if (command.getName().equalsIgnoreCase("testutils") && args[0].equalsIgnoreCase("deleteblock")) {
 				return this.getBlocks(args[1], (Player) sender);
-			} else if (command.getName().equalsIgnoreCase("testblock") && args[0].equalsIgnoreCase("here")) {
-				return this.getBlocks(args[1], (Player) sender);
+			} else if (command.getName().equalsIgnoreCase("testblock")) {
+				if (args[0].equalsIgnoreCase("here")) {
+					return this.getBlocks(args[1], (Player) sender);
+				} else {
+					return this.getCompletions("here");
+				}
 			}
 		}
 		return Collections.emptyList();
