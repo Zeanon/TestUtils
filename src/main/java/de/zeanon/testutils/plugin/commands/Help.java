b234341f4e72cc12ad35b292b4966eff167c7a9b
Zeanon <thezeanon@gmail.com>
2021-02-18 18:40:32 +0100

    small update
    Signed-off-by: Zeanon <thezeanon@gmail.com>

--- src/main/java/de/zeanon/testutils/plugin/commands/Help.java
+++ src/main/java/de/zeanon/testutils/plugin/commands/Help.java
@@ -1,6 +1,15 @@
 package de.zeanon.testutils.plugin.commands;
 
-@SuppressWarnings("unused")
+import lombok.experimental.UtilityClass;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+
+@UtilityClass
 public class Help {
 
+	@SuppressWarnings("unused")
+	public void execute(final @NotNull Player p) {
+		//not implemented yet
+	}
 }
