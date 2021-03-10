b368b96fe0324e6af7ea809ce09b7354774bc4b8
Zeanon <thezeanon@gmail.com>
2021-03-10 20:01:34 +0100

    Added region based Stoplag
    Signed-off-by: Zeanon <thezeanon@gmail.com>

---
+++ src/main/java/de/zeanon/testutils/plugin/commands/Stoplag.java
@@ -0,0 +1,155 @@
+package de.zeanon.testutils.plugin.commands;
+
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import de.zeanon.storagemanagercore.external.browniescollections.GapList;
+import de.zeanon.storagemanagercore.internal.utility.basic.Objects;
+import de.zeanon.testutils.init.InitMode;
+import de.zeanon.testutils.plugin.utils.GlobalMessageUtils;
+import de.zeanon.testutils.plugin.utils.TestAreaUtils;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.experimental.UtilityClass;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+
+@UtilityClass
+public class Stoplag {
+
+	private final List<ProtectedRegion> stoplagRegions = new GapList<>();
+
+
+	public void execute(final @NotNull String[] args, final @NotNull Player p) {
+		if (args.length == 1) {
+			ProtectedRegion tempRegion = TestAreaUtils.getRegion(p);
+			if (!Stoplag.stoplagRegions.contains(tempRegion)) {
+				Stoplag.stoplagRegions.add(tempRegion);
+				Stoplag.stoplagRegions.add(TestAreaUtils.getOppositeRegion(p));
+			}
+			p.sendMessage(GlobalMessageUtils.messageHead
+						  + ChatColor.RED + "Stoplag has been " + ChatColor.GREEN + "activated" + ChatColor.RED + " in your TestArea.");
+		} else if (args.length == 2) {
+			if (args[1].equalsIgnoreCase("-c")) {
+				ProtectedRegion tempRegion = TestAreaUtils.getRegion(p);
+
+				if (tempRegion == null) {
+					GlobalMessageUtils.sendNotApplicableRegion(p);
+				} else {
+					Stoplag.stoplagRegions.remove(tempRegion);
+					Stoplag.stoplagRegions.remove(TestAreaUtils.getOppositeRegion(p));
+					p.sendMessage(GlobalMessageUtils.messageHead
+								  + ChatColor.RED + "Stoplag has been deactivated in your TestArea.");
+				}
+			} else if (args[1].equalsIgnoreCase("here")) {
+				ProtectedRegion tempRegion = TestAreaUtils.getRegion(p);
+
+				if (tempRegion == null) {
+					GlobalMessageUtils.sendNotApplicableRegion(p);
+				} else {
+					Stoplag.stoplagRegions.add(tempRegion);
+					p.sendMessage(GlobalMessageUtils.messageHead
+								  + ChatColor.RED + "Stoplag has been " + ChatColor.GREEN + "activated" + ChatColor.RED + " on your side of your TestArea.");
+				}
+			} else if (args[1].equalsIgnoreCase("other")) {
+				ProtectedRegion tempRegion = TestAreaUtils.getOppositeRegion(p);
+
+				if (tempRegion == null) {
+					GlobalMessageUtils.sendNotApplicableRegion(p);
+				} else {
+					Stoplag.stoplagRegions.add(tempRegion);
+					p.sendMessage(GlobalMessageUtils.messageHead
+								  + ChatColor.RED + "Stoplag has been " + ChatColor.GREEN + "activated" + ChatColor.RED + " on the other side of your TestArea.");
+				}
+			} else if (args[1].equalsIgnoreCase("global")) {
+				p.performCommand("stoplag confirm");
+			}
+		} else if (args.length == 3) {
+			if (args[2].equalsIgnoreCase("-c")) {
+				if (args[1].equalsIgnoreCase("global")) {
+					p.performCommand("stoplag -c");
+				} else if (args[1].equalsIgnoreCase("here")) {
+					ProtectedRegion tempRegion = TestAreaUtils.getRegion(p);
+
+					if (tempRegion == null) {
+						GlobalMessageUtils.sendNotApplicableRegion(p);
+					} else {
+						Stoplag.stoplagRegions.remove(tempRegion);
+						p.sendMessage(GlobalMessageUtils.messageHead
+									  + ChatColor.RED + "Stoplag has been deactivated on your side of your TestArea.");
+					}
+				} else if (args[1].equalsIgnoreCase("other")) {
+					ProtectedRegion tempRegion = TestAreaUtils.getOppositeRegion(p);
+
+					if (tempRegion == null) {
+						GlobalMessageUtils.sendNotApplicableRegion(p);
+					} else {
+						Stoplag.stoplagRegions.remove(tempRegion);
+						p.sendMessage(GlobalMessageUtils.messageHead
+									  + ChatColor.RED + "Stoplag has been deactivated on the other side  of your TestArea.");
+					}
+				}
+			} else {
+				p.sendMessage(GlobalMessageUtils.messageHead
+							  + net.md_5.bungee.api.ChatColor.RED + "Too many arguments.");
+			}
+		} else {
+			p.sendMessage(GlobalMessageUtils.messageHead
+						  + net.md_5.bungee.api.ChatColor.RED + "Too many arguments.");
+		}
+	}
+
+	public @NotNull List<String> onTab(final @NotNull String buffer) {
+		final @NotNull String message = buffer.replaceAll("\\s+", " ");
+		final @NotNull String[] args = buffer.replaceAll("\\s+", " ").split(" ");
+		final boolean argumentEnded = message.endsWith(" ");
+		if ((args.length == 2 && !argumentEnded) || (args.length == 1 && argumentEnded)) {
+			if (argumentEnded) {
+				return Arrays.asList("-c", "here", "other", "global");
+			} else {
+				return Stoplag.getCompletions(args[1], "-c", "here", "other", "global");
+			}
+		} else if (args.length == 3 && !argumentEnded || args.length == 2) {
+			if (argumentEnded) {
+				if (!args[1].equalsIgnoreCase("-c")) {
+					return Collections.singletonList("-c");
+				} else {
+					return Collections.emptyList();
+				}
+			} else {
+				if (!args[1].equalsIgnoreCase("-c")) {
+					return Stoplag.getCompletions(args[2], "-c");
+				} else {
+					return Collections.emptyList();
+				}
+			}
+		} else {
+			return Collections.emptyList();
+		}
+	}
+
+	public boolean inStoplagRegion(final @NotNull Location location) {
+		final @NotNull RegionManager tempManager = Objects.notNull(InitMode.getRegionContainer()
+																		   .get(new BukkitWorld(location.getWorld())));
+		for (ProtectedRegion temp : tempManager.getApplicableRegions(BlockVector3.at(location.getX(),
+																					 location.getY(),
+																					 location.getZ()))) {
+			if (Stoplag.stoplagRegions.contains(temp)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private List<String> getCompletions(final @NotNull String arg, final @NotNull String... completions) {
+		return Arrays.stream(completions)
+					 .filter(completion -> completion.startsWith(arg.toLowerCase()))
+					 .collect(Collectors.toList());
+	}
+}
--- src/main/java/de/zeanon/testutils/plugin/handlers/EventListener.java
+++ src/main/java/de/zeanon/testutils/plugin/handlers/EventListener.java
@@ -1,22 +1,31 @@
 package de.zeanon.testutils.plugin.handlers;
 
+import de.zeanon.storagemanagercore.internal.utility.basic.Objects;
 import de.zeanon.testutils.TestUtils;
+import de.zeanon.testutils.plugin.commands.Stoplag;
 import de.zeanon.testutils.plugin.update.Update;
 import de.zeanon.testutils.plugin.utils.GlobalMessageUtils;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.*;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.event.server.TabCompleteEvent;
 import org.jetbrains.annotations.NotNull;
 
 
 public class EventListener implements Listener {
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGH)
 	public void onCommand(PlayerCommandPreprocessEvent event) {
-		final @NotNull String[] args = event.getMessage().replace("worldguard:", "/").split("\\s+");
+		final @NotNull String[] args = event.getMessage().replace("worldguard:", "").split("\\s+");
 		if ((args[0].equalsIgnoreCase("/rg") || args[0].equalsIgnoreCase("/region"))
 			&& (args[1].equalsIgnoreCase("define") || args[1].equalsIgnoreCase("d") || args[1].equalsIgnoreCase("create"))
 			&& args[2].toLowerCase().startsWith("testarea_")
@@ -28,6 +37,16 @@ public class EventListener implements Listener {
 										  + ChatColor.RED + "' and ends with '"
 										  + ChatColor.DARK_RED + args[2].substring(args[2].length() - 6)
 										  + "'.");
+		} else if (args[0].equalsIgnoreCase("/stoplag")) {
+			event.setCancelled(true);
+			Stoplag.execute(args, event.getPlayer());
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onTab(final @NotNull TabCompleteEvent event) {
+		if (event.getBuffer().toLowerCase().startsWith("/stoplag")) {
+			event.setCompletions(Stoplag.onTab(event.getBuffer()));
 		}
 	}
 
@@ -43,6 +62,94 @@ public class EventListener implements Listener {
 			|| event.getPlugin().getName().equalsIgnoreCase("WorldGuard")) {
 			TestUtils.getPluginManager().disablePlugin(TestUtils.getInstance());
 			TestUtils.getPluginManager().enablePlugin(TestUtils.getInstance());
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onBlockFromTo(BlockFromToEvent event) {
+		if (Stoplag.inStoplagRegion(event.getBlock().getLocation()) || Stoplag.inStoplagRegion(event.getToBlock().getLocation())) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onBlockIgnite(BlockIgniteEvent event) {
+		if (Stoplag.inStoplagRegion(Objects.notNull(event.getIgnitingBlock()).getLocation())) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onBlockBurn(BlockBurnEvent event) {
+		if (Stoplag.inStoplagRegion(Objects.notNull(event.getIgnitingBlock()).getLocation())) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	public void onBlockPhysics(BlockPhysicsEvent event) {
+		if (Stoplag.inStoplagRegion(event.getBlock().getLocation())) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onLeavesDecay(LeavesDecayEvent event) {
+		if (Stoplag.inStoplagRegion(event.getBlock().getLocation())) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onBlockForm(BlockFormEvent event) {
+		if (Stoplag.inStoplagRegion(event.getBlock().getLocation())) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onBlockSpread(BlockSpreadEvent event) {
+		if (Stoplag.inStoplagRegion(event.getSource().getLocation()) || Stoplag.inStoplagRegion(event.getBlock().getLocation())) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	public void onBlockExplode(BlockExplodeEvent event) {
+		boolean remove = false;
+		for (final @NotNull Block block : event.blockList()) {
+			if (Stoplag.inStoplagRegion(block.getLocation())) {
+				remove = true;
+				break;
+			}
+		}
+
+		if (remove || Stoplag.inStoplagRegion(event.getBlock().getLocation())) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onEntityExplode(EntityExplodeEvent event) {
+		if (Stoplag.inStoplagRegion(event.getLocation())) {
+			event.getEntity().remove();
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onExplosionPrime(ExplosionPrimeEvent event) {
+		if (Stoplag.inStoplagRegion(event.getEntity().getLocation())) {
+			event.getEntity().remove();
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onCreatureSpawn(CreatureSpawnEvent event) {
+		if (Stoplag.inStoplagRegion(event.getLocation())) {
+			event.getEntity().remove();
+			event.setCancelled(true);
 		}
 	}
 }
