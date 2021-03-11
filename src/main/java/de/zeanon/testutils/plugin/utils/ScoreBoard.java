afa68cfbf4ca7ab56797c4e16f57ce3da847c71f
Zeanon <thezeanon@gmail.com>
2021-03-11 01:50:01 +0100

    fixed scoreboard flickering
    Signed-off-by: Zeanon <thezeanon@gmail.com>

--- src/main/java/de/zeanon/testutils/plugin/utils/ScoreBoard.java
+++ src/main/java/de/zeanon/testutils/plugin/utils/ScoreBoard.java
@@ -30,11 +30,24 @@ public class ScoreBoard {
 	public void execute(final @NotNull Player p) {
 		if (!ScoreBoard.scoreBoards.contains(p.getUniqueId().toString())) {
 			ScoreBoard.scoreBoards.add(p.getUniqueId().toString());
+
+			final @Nullable ProtectedRegion tempRegion = TestAreaUtils.getRegion(p);
+			if (tempRegion != null) {
+				ScoreBoard.setScoreBoard(p, tempRegion);
+			} else {
+				p.setScoreboard(Objects.notNull(ScoreBoard.scoreboardManager).getNewScoreboard());
+			}
+
 			new BukkitRunnable() {
 				@Override
 				public void run() {
 					if (Bukkit.getOnlinePlayers().contains(p)) {
-						ScoreBoard.setScoreBoard(p);
+						final @Nullable ProtectedRegion tempRegion = TestAreaUtils.getRegion(p);
+						if (tempRegion != null) {
+							ScoreBoard.updateScoreBoard(p, tempRegion);
+						} else {
+							p.setScoreboard(Objects.notNull(ScoreBoard.scoreboardManager).getNewScoreboard());
+						}
 					} else {
 						ScoreBoard.scoreBoards.remove(p.getUniqueId().toString());
 						this.cancel();
@@ -44,39 +57,182 @@ public class ScoreBoard {
 		}
 	}
 
-	private void setScoreBoard(final @NotNull Player p) {
+	private void setScoreBoard(final @NotNull Player p, final @NotNull ProtectedRegion tempRegion) {
 		final @NotNull Scoreboard scoreboard = Objects.notNull(ScoreBoard.scoreboardManager).getNewScoreboard();
-		final @Nullable ProtectedRegion tempRegion = TestAreaUtils.getRegion(p);
-		if (tempRegion != null) {
-			final @NotNull Objective infoBoard = scoreboard.registerNewObjective("infoBoard", "dummy", ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "[" + ChatColor.DARK_RED + "" + ChatColor.BOLD + TestUtils.getInstance().getName() + ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "]");
 
-			final @NotNull Score header = infoBoard.getScore(ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "!================!");
-			header.setScore(7);
+		final @NotNull Objective infoBoard = scoreboard.registerNewObjective("infoBoard",
+																			 "dummy",
+																			 ChatColor.DARK_GRAY
+																			 + ""
+																			 + ChatColor.BOLD
+																			 + "["
+																			 + ChatColor.DARK_RED
+																			 + ""
+																			 + ChatColor.BOLD
+																			 + TestUtils.getInstance().getName()
+																			 + ChatColor.DARK_GRAY
+																			 + ""
+																			 + ChatColor.BOLD
+																			 + "]");
 
-			final @NotNull Score area = infoBoard.getScore(ChatColor.DARK_GRAY + " [" + ChatColor.DARK_RED + "TestArea" + ChatColor.DARK_GRAY + "]");
-			area.setScore(6);
+		infoBoard.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-			final @NotNull Score areaName = infoBoard.getScore(ChatColor.DARK_GRAY + "   " + ChatColor.BOLD + ">> " + ChatColor.RED + tempRegion.getId().substring(9, tempRegion.getId().length() - 6) + ChatColor.DARK_GRAY + "" + ChatColor.BOLD + " <<");
-			areaName.setScore(5);
+		final @NotNull Score header = infoBoard.getScore(ChatColor.DARK_GRAY
+														 + ""
+														 + ChatColor.BOLD
+														 + "!================!");
+		header.setScore(9);
 
-			final @NotNull Score tnt = infoBoard.getScore(ChatColor.DARK_GRAY + " [" + ChatColor.DARK_RED + "TNT" + ChatColor.DARK_GRAY + "]");
-			tnt.setScore(4);
+		final @NotNull Score area = infoBoard.getScore(ChatColor.DARK_GRAY
+													   + " ["
+													   + ChatColor.DARK_RED
+													   + "TestArea"
+													   + ChatColor.DARK_GRAY
+													   + "]");
+		area.setScore(8);
 
-			final @NotNull Score tntInfo = infoBoard.getScore(ChatColor.DARK_GRAY + "   " + ChatColor.BOLD + ">> " + (tempRegion.getFlag(Flags.TNT) == StateFlag.State.ALLOW ? ChatColor.GREEN + "allow" : ChatColor.RED + "deny") + ChatColor.DARK_GRAY + "" + ChatColor.BOLD + " <<");
-			tntInfo.setScore(3);
+		final @NotNull Team areaName = scoreboard.registerNewTeam("areaname");
+		areaName.addEntry(ChatColor.AQUA + "" + ChatColor.AQUA);
+		areaName.setPrefix(ChatColor.DARK_GRAY
+						   + "   "
+						   + ChatColor.BOLD
+						   + ">> "
+						   + ChatColor.RED
+						   + tempRegion.getId().substring(9, tempRegion.getId().length() - 6)
+						   + ChatColor.DARK_GRAY
+						   + ""
+						   + ChatColor.BOLD
+						   + " <<");
+		infoBoard.getScore(ChatColor.AQUA + "" + ChatColor.AQUA).setScore(7);
 
-			final @NotNull Score stoplag = infoBoard.getScore(ChatColor.DARK_GRAY + " [" + ChatColor.DARK_RED + "Stoplag" + ChatColor.DARK_GRAY + "]");
-			stoplag.setScore(2);
+		final @NotNull Score side = infoBoard.getScore(ChatColor.DARK_GRAY
+													   + " ["
+													   + ChatColor.DARK_RED
+													   + "Side"
+													   + ChatColor.DARK_GRAY
+													   + "]");
+		side.setScore(6);
 
-			final @NotNull Score stoplagInfo = infoBoard.getScore(ChatColor.DARK_GRAY + "   " + ChatColor.BOLD + ">> " + (Stoplag.isStopLagRegion(tempRegion) ? ChatColor.GREEN + "activated" : ChatColor.RED + "deactivated") + ChatColor.DARK_GRAY + "" + ChatColor.BOLD + " <<");
-			stoplagInfo.setScore(1);
+		final @NotNull Team sideName = scoreboard.registerNewTeam("sidename");
+		sideName.addEntry(ChatColor.BLACK + "" + ChatColor.BLACK);
+		sideName.setPrefix(ChatColor.DARK_GRAY
+						   + "   "
+						   + ChatColor.BOLD
+						   + ">> "
+						   + ChatColor.RED
+						   + tempRegion.getId().substring(tempRegion.getId().length() - 5)
+						   + ChatColor.DARK_GRAY
+						   + ""
+						   + ChatColor.BOLD
+						   + " <<");
+		infoBoard.getScore(ChatColor.BLACK + "" + ChatColor.BLACK).setScore(5);
 
-			final @NotNull Score footer = infoBoard.getScore(ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "¡================¡");
-			footer.setScore(0);
+		final @NotNull Score tnt = infoBoard.getScore(ChatColor.DARK_GRAY
+													  + " ["
+													  + ChatColor.DARK_RED
+													  + "TNT"
+													  + ChatColor.DARK_GRAY
+													  + "]");
+		tnt.setScore(4);
 
-			infoBoard.setDisplaySlot(DisplaySlot.SIDEBAR);
-		}
+		final @NotNull Team tntInfo = scoreboard.registerNewTeam("tntinfo");
+		tntInfo.addEntry(ChatColor.BLUE + "" + ChatColor.BLUE);
+		tntInfo.setPrefix(ChatColor.DARK_GRAY
+						  + "   "
+						  + ChatColor.BOLD
+						  + ">> "
+						  + (tempRegion.getFlag(Flags.TNT) == StateFlag.State.ALLOW
+							 ? ChatColor.GREEN + "allow"
+							 : ChatColor.RED + "deny")
+						  + ChatColor.DARK_GRAY
+						  + ""
+						  + ChatColor.BOLD
+						  + " <<");
+		infoBoard.getScore(ChatColor.BLUE + "" + ChatColor.BLUE).setScore(3);
+
+		final @NotNull Score stoplag = infoBoard.getScore(ChatColor.DARK_GRAY
+														  + " ["
+														  + ChatColor.DARK_RED
+														  + "Stoplag"
+														  + ChatColor.DARK_GRAY
+														  + "]");
+		stoplag.setScore(2);
+
+		final @NotNull Team stoplagInfo = scoreboard.registerNewTeam("stoplaginfo");
+		stoplagInfo.addEntry(ChatColor.BOLD + "" + ChatColor.BOLD);
+		stoplagInfo.setPrefix(ChatColor.DARK_GRAY
+							  + "   "
+							  + ChatColor.BOLD
+							  + ">> "
+							  + (Stoplag.isStopLagRegion(tempRegion)
+								 ? ChatColor.GREEN + "activated"
+								 : ChatColor.RED + "deactivated")
+							  + ChatColor.DARK_GRAY
+							  + ""
+							  + ChatColor.BOLD
+							  + " <<");
+		infoBoard.getScore(ChatColor.BOLD + "" + ChatColor.BOLD).setScore(1);
+
+		final @NotNull Score footer = infoBoard.getScore(ChatColor.DARK_GRAY
+														 + ""
+														 + ChatColor.BOLD
+														 + "¡================¡");
+		footer.setScore(0);
 
 		p.setScoreboard(scoreboard);
+	}
+
+	private void updateScoreBoard(final @NotNull Player p, final @NotNull ProtectedRegion tempRegion) {
+		final @NotNull Scoreboard scoreboard = p.getScoreboard();
+
+		if (scoreboard.getObjective("infoBoard") != null) {
+			Objects.notNull(scoreboard.getTeam("areaname"))
+				   .setPrefix(ChatColor.DARK_GRAY + "   "
+							  + ChatColor.BOLD
+							  + ">> "
+							  + ChatColor.RED
+							  + tempRegion.getId().substring(9, tempRegion.getId().length() - 6)
+							  + ChatColor.DARK_GRAY
+							  + ""
+							  + ChatColor.BOLD
+							  + " <<");
+			Objects.notNull(scoreboard.getTeam("sidename"))
+				   .setPrefix(ChatColor.DARK_GRAY
+							  + "   "
+							  + ChatColor.BOLD
+							  + ">> "
+							  + ChatColor.RED
+							  + tempRegion.getId().substring(tempRegion.getId().length() - 5)
+							  + ChatColor.DARK_GRAY
+							  + ""
+							  + ChatColor.BOLD
+							  + " <<");
+			Objects.notNull(scoreboard.getTeam("tntinfo"))
+				   .setPrefix(ChatColor.DARK_GRAY
+							  + "   "
+							  + ChatColor.BOLD
+							  + ">> "
+							  + (tempRegion.getFlag(Flags.TNT) == StateFlag.State.ALLOW
+								 ? ChatColor.GREEN + "allow"
+								 : ChatColor.RED + "deny")
+							  + ChatColor.DARK_GRAY
+							  + ""
+							  + ChatColor.BOLD
+							  + " <<");
+			Objects.notNull(scoreboard.getTeam("stoplaginfo"))
+				   .setPrefix(ChatColor.DARK_GRAY
+							  + "   "
+							  + ChatColor.BOLD
+							  + ">> "
+							  + (Stoplag.isStopLagRegion(tempRegion)
+								 ? ChatColor.GREEN + "activated"
+								 : ChatColor.RED + "deactivated")
+							  + ChatColor.DARK_GRAY
+							  + ""
+							  + ChatColor.BOLD
+							  + " <<");
+		} else {
+			ScoreBoard.setScoreBoard(p, tempRegion);
+		}
 	}
 }
